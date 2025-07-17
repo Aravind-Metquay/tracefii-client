ab3d84e188725152e3db0d8dd0f7a2d36c0937b7
adithya <adithyav@metquay.com>
2025-07-17 10:15:29 +0530

    added component routes and css

---
+++ src/routes/components/checkbox/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Checkbox from "$lib/components/ui/checkbox/checkbox.svelte";
+</script>
+
+<div class="flex items-center justify-center gap-2 h-screen">
+	<Checkbox checked={true}/>
+	<Checkbox checked={false}/>
+	<Checkbox checked={false} indeterminate={true} />
+	<Checkbox disabled={true} />
+</div>
