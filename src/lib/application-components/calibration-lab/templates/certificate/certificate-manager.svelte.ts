c1d15da5e4f7ee8c0c46a6e5d7fbf123bc335ee9
Prayag <prayag@metquay.com>
2025-08-07 12:31:30 +0530

    modified data model

--- src/lib/application-components/calibration-lab/templates/certificate/certificate-manager.svelte.ts
+++ src/lib/application-components/calibration-lab/templates/certificate/certificate-manager.svelte.ts
@@ -1,21 +1,62 @@
 import type { WorksheetType } from '@/Types';
+import * as fabric from 'fabric';
+import type { WorkType } from '@/Types';
 
 interface Header {
-    height : number;
-    unit : 'in' |  'mm' | 'cm' | 'px';
-    setAs : 'First Page Header' | 'Last Page Header' | 'Default Header';
-    canvasData : any;
+	height: number;
+	unit: 'in' | 'mm' | 'cm' | 'px';
+	setAs: 'First Page Header' | 'Last Page Header' | 'Default Header';
+	canvasData: fabric.Canvas;
 }
 
 interface Footer {
-    height : number;
-    unit : 'in' |  'mm' | 'cm' | 'px';
-    setAs : 'First Page Footer' | 'Last Page Footer' | 'Default Footer';
-    canvasData : any;
+	height: number;
+	unit: 'in' | 'mm' | 'cm' | 'px';
+	setAs: 'First Page Footer' | 'Last Page Footer' | 'Default Footer';
+	canvasData: fabric.Canvas;
 }
 
 interface Content {
-    
+	order: number;
+	ComponentType:
+		| 'Custom Field'
+		| 'Reference Instrument'
+		| 'Calibration Data'
+		| 'Customer and Instrument Details';
+	CalibrationData?: CalibrationData;
+	CustomField?: CustomField;
+	ReferenceInstrument?: ReferenceInstrument;
+	CustomerAndInstrumentDetails?: CustomerAndInstrumentDetails;
+}
+interface CalibrationData {
+	// Pending types [ Empty]
+}
+interface ReferenceInstrument {
+	format: 'Table';
+	title?: string;
+	masterInstrument?: boolean;
+	columns: Record<
+		string, // needs to change to enum later - reference instruments
+		{
+			isActive: boolean;
+			order: number;
+		}
+	>; // Might want to convert into array, check for enum
+}
+interface CustomerAndInstrumentDetails {
+	noOfColumns: 1 | 2;
+	fields: Record<
+		string, //  needs to change to enum based on customer and instrument details
+		{
+			isActive: boolean;
+			columns: 1 | 2;
+		}
+	>;
+}
+interface CustomField {
+	id: string;
+	title: string;
+	canvasData: fabric.Canvas;
 }
 
 interface CertificateTemplate {
@@ -27,7 +68,7 @@ interface CertificateTemplate {
 	dimensions: {
 		width: number;
 		height: number;
-		unit: 'in' |  'mm' | 'cm' | 'px';
+		unit: 'in' | 'mm' | 'cm' | 'px';
 	};
 	margin: {
 		top: number;
@@ -40,23 +81,38 @@ interface CertificateTemplate {
 	isTemplateBilingual: boolean;
 
 	headers: {
-        firstPageHeader : Header
-        lastPageHeader : Header
-        defaultHeader : Header
-    };
+		firstPageHeader: Header;
+		lastPageHeader: Header;
+		defaultHeader: Header;
+	};
 	footers: {
-        firstPageFooter : Footer
-        lastPageFooter : Footer
-        defaultFooter : Footer
-    }
+		firstPageFooter: Footer;
+		lastPageFooter: Footer;
+		defaultFooter: Footer;
+	};
 	contents: Content[];
 }
 
-interface CertificateManager {
-    getCertificate : () => CertificateTemplate;
+interface CertificateTemplateManager {
+	getCertificate: () => CertificateTemplate;
+	savePDF: () => void;
+	updateFormat: (format?: 'A4' | 'Custom' | 'A5') => void;
+	updateUnit: (unit?: 'mm' | 'cm' | 'in' | 'px') => void;
+	setDimensions: (width?: number, height?: number) => void;
+	setMargins: (top: number, right: number, bottom: number, left: number) => void;
+	updateWork: (work: WorkType) => void;
+	updateHeaderHeight: (value?: number) => void;
+	updateFooterHeight: (value?: number) => void;
+	updateCertificateTemplateName: () => void;
+	updateHeader: () => void;
+	updateFooter: () => void;
+	updateContent: () => void;
+	updateCustomField?: () => void;
+	updateReferenceInstrumentTitle?: () => void;
+	updateCustomerAndInstrumentDetails?: () => void;
 }
 
-export function CertificateManager() : CertificateManager {
+export function CertificateManager(): CertificateTemplateManager {
 	let certificateState = $state<CertificateTemplate>({
 		certificateTemplateId: '',
 		certificateTemplateName: '',
@@ -78,21 +134,42 @@ export function CertificateManager() : CertificateManager {
 		isTemplateBilingual: false,
 
 		headers: {
-            firstPageHeader : {},
-            lastPageHeader : {},
-            defaultHeader : {},
-        },
+			firstPageHeader: {},
+			lastPageHeader: {},
+			defaultHeader: {}
+		},
 		footers: {
-            firstPageFooter : {},
-            lastPageFooter : {},
-            defaultFooter : {}
-        },
+			firstPageFooter: {},
+			lastPageFooter: {},
+			defaultFooter: {}
+		},
 		contents: []
 	});
 
+	const updateCustomerAndInstrumentDetails = (title?: string, columValue?: number) => {
+		const changeTitle = () => {};
+
+		const changeColumn = () => {};
+		return {
+			changeTitle,
+			changeColumn
+		};
+	};
+
+	const updateReferenceInstrumentTitle = (title?: string, columValue?: number) => {
+		const changeTitle = () => {};
+
+		const changeColumn = () => {};
+		return {
+			changeTitle,
+			changeColumn
+		};
+	};
+
 	return {
-        getCertificate() {
-            return certificateState
-        },
-    };
+		getCertificate() {
+			return certificateState;
+		},
+		updateReferenceInstrumentTitle
+	};
 }
